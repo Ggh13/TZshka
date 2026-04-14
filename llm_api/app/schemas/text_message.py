c4097839200e220406b32f656309e0cdf9875755
GermanPikel <germanpikel@gmail.com>
2026-04-14 20:26:25 +0300

    change textinputschema

--- llm_api/app/schemas/text_message.py
+++ llm_api/app/schemas/text_message.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel, Field, ConfigDict
 
 
 class TextInput(BaseModel):
-    content: str = Field(min_length=1, max_length=10000, description="Текст технического задания")
+    content: str = Field(min_length=1, description="Текст технического задания")
 
     model_config = ConfigDict(from_attributes=True)
