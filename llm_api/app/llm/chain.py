f353afaa90d89107b38472ab5a534abb1b8845b7
GermanPikel <germanpikel@gmail.com>
2026-04-14 20:15:47 +0300

    upd

---
+++ llm_api/app/llm/chain.py
@@ -0,0 +1,56 @@
+import os
+from langchain_core.prompts import (
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate
+)
+from app.llm.model import llm
+
+from pathlib import Path
+
+rules_path = Path(__file__).parent.parent / 'static_files' / 'rules.txt'
+
+with open(rules_path, 'r', encoding='utf-8') as file:
+    rules = file.read()
+
+sp = """
+Ты полезный ассистент. Твоя роль заключается в том, чтобы анализировать 
+технические задания пользователей и находить в них несоответствия правилам 
+составления технических заданий. 
+Все правила составления хорошего технического задания описаны в списке: 
+""" + rules + """
+Ты должен проанализировать техническое задание и проверить его на соответствие 
+КАЖДОМУ правилу из списка. 
+В случае возникновения противоречия какому либо правилу составления хорошего 
+технического задания ты должен вывести сообщение с правилом, которое нарушено, 
+и частью (частями) текста, в котором есть противоречие. Учти, что таких нарушений 
+может быть насколько в одном техническом задании пользователя. Также ты должен 
+дать объективную оценку качества технического задания.
+"""
+
+system_prompt = SystemMessagePromptTemplate.from_template(sp)
+
+user_prompt = HumanMessagePromptTemplate.from_template(
+    """
+    Техническое задание для анализа:
+
+    {technical_specification}
+
+    """,
+    input_variables=["technical_specification"]
+)
+
+chat_prompt = ChatPromptTemplate.from_messages([system_prompt, user_prompt])
+
+CHAIN_TEXT_HANDLER = (
+    {
+        'technical_specification': lambda x: x['technical_specification']
+    }
+    | chat_prompt
+    | llm
+    | {
+        "status": lambda x: x.status,
+        "issues": lambda x: x.issues,
+        "feedback": lambda x: x.feedback
+    }
+)
--- llm_api/app/routers/llm.py
+++ llm_api/app/routers/llm.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, status
 from app.schemas.text_message import TextInput as TextInputSchema
 from app.schemas.llm_output import LLMOutput as LLMOutputSchema
-from app.llm.model import llm
+from app.llm.chain import CHAIN_TEXT_HANDLER as LLM
 
 
 router = APIRouter(
@@ -10,13 +10,13 @@ router = APIRouter(
 )
 
 
-@router.get('/text', response_model=LLMOutputSchema)
+@router.post('/text', response_model=LLMOutputSchema)
 async def get_response_by_text(
     payload: TextInputSchema
 ):
     user_content = payload.content
     try:
-        llm_output = llm.invoke(user_content)
+        llm_output = LLM.invoke(user_content)
     except:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="LLM is not available")
 
