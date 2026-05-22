73bc0d3f32fe3992eb7c2fcae003a2b13c3fa054
GermanPikel <germanpikel@gmail.com>
2026-05-22 12:09:18 +0300

    removed max_tokens parameter in llms

--- llm_api/app/llm/model.py
+++ llm_api/app/llm/model.py
@@ -10,14 +10,12 @@ base_llm = ChatOpenRouter(
     # model="meta-llama/llama-3.3-70b-instruct",
     model="qwen/qwen3-next-80b-a3b-instruct",
     temperature=0,
-    max_tokens=11865,
     api_key=os.getenv("OPENROUTER_API_KEY")
 )
 
 thinking_base_llm = ChatOpenRouter(
     model="qwen/qwen3-next-80b-a3b-instruct",
-    temperature=0.5,
-    max_tokens=11865,
+    temperature=0.3,
     api_key=os.getenv("OPENROUTER_API_KEY")
 )
 
